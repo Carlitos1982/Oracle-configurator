7baa5b99e58ee9f831e280a8b8e20d700499482e
Carlitos1982 <dzecchinel@gmail.com>
2025-05-16 15:21:39 +0200

    Update Oracle_app.py

--- Oracle_app.py
+++ Oracle_app.py
@@ -111,57 +111,6 @@ size_df = data["size_df"]
 features_df = data["features_df"]
 materials_df = data["materials_df"]
 
-def genera_output_flange():
-    pipe_type = st.selectbox("Pipe type", ["SW", "WN"])
-    size = st.selectbox(
-        "Size",
-        ['1/8”', '1/4”', '3/8”', '1/2”', '3/4”', '1”', '1-1/4”', '1-1/2”', '2”', '2-1/2”', '3”', '4”']
-    )
-    face_type = st.selectbox("Face type", ["RF", "FF", "RJ"])
-part_options = [
-    "Baseplate, Pump",
-    "Casing, Pump",
-    "Casing Cover, Pump",
-    "Impeller, Pump",
-    "Balance Bushing, Pump",
-    "Balance Drum, Pump",
-    "Balance Disc, Pump",
-    "Shaft, Pump",
-    "Flange, Pipe"
-]
-selected_part = st.selectbox("Seleziona Parte", part_options)
-
-pump_models = sorted(size_df["Pump Model"].dropna().unique())
-material_types = materials_df["Material Type"].dropna().unique().tolist()
-
-# ROUTING
-if selected_part == "Flange, Pipe":
-    st.subheader("Configurazione - Flange, Pipe")
-    genera_output_flange()
-elif selected_part == "Baseplate, Pump":
-    st.subheader("Configurazione - Baseplate, Pump")
-    genera_output("baseplate", "477...", "6110-BASE PLATE", "", "ARTVARI", "18_FOUNDATION PLATE", extra_fields="baseplate")
-elif selected_part == "Casing, Pump":
-    st.subheader("Configurazione - Casing, Pump")
-    genera_output("casing", "40202...", "1100-CASING", "3", "CORPO", "17_CASING", template_fisso="FPD_MAKE")
-elif selected_part == "Casing Cover, Pump":
-    st.subheader("Configurazione - Casing Cover, Pump")
-    genera_output("cover", "40205...", "1221-CASING COVER", "3", "COPERCHIO", "13_OTHER")
-elif selected_part == "Impeller, Pump":
-    st.subheader("Configurazione - Impeller, Pump")
-    genera_output("imp", "40229...", "2200-IMPELLER", "2-3", "GIRANTE", "20_IMPELLER_DIFFUSER", template_fisso="FPD_MAKE")
-elif selected_part == "Balance Bushing, Pump":
-    st.subheader("Configurazione - Balance Bushing, Pump")
-    genera_output("balance", "40226...", "6231-BALANCE DRUM BUSH", "1-2-3", "ALBERO", "16_BUSHING", extra_fields="diameters")
-elif selected_part == "Balance Drum, Pump":
-    st.subheader("Configurazione - Balance Drum, Pump")
-    genera_output("drum", "40227...", "6231-BALANCE DRUM BUSH", "1-2-3", "ARTVARI", "16_BUSHING", extra_fields="diameters")
-elif selected_part == "Balance Disc, Pump":
-    st.subheader("Configurazione - Balance Disc, Pump")
-    genera_output("disc", "40228...", "6210-BALANCE DISC", "1-2-3", "ARTVARI", "30_DISK", extra_fields="diameters")
-elif selected_part == "Shaft, Pump":
-    st.subheader("Configurazione - Shaft, Pump")
-    genera_output("shaft", "40231...", "2100-SHAFT", "2-3", "ALBERO", "25_SHAFTS", template_fisso="FPD_MAKE", extra_fields="shaft")
 
 # OUTPUT
 if "output_data" in st.session_state:
@@ -190,25 +139,4 @@ if "output_data" in st.session_state:
 
     note = st.text_area("Note (opzionale)", height=80)
 
-    if st.button("Genera Output"):
-        description = (
-            f"Flange, Pipe type {pipe_type} Size {size} "
-            f"Face type {face_type} Class {class_flange} Material {material}"
-        )
-        if note:
-            description += f" Note: {note}"
-        st.session_state["output_data"] = {
-            "Item": "50155…",
-            "Description": description,
-            "Identificativo": "1245-FLANGE",
-            "Classe ricambi": "",
-            "Categories": "Fascia ite 5",
-            "Catalog": "",
-            "Material": "NOT AVAILABLE",
-            "FPD material code": "NA",
-            "Template": "FPD_BUY_2",
-            "ERP_L1": "23_FLANGE",
-            "ERP_L2": "13_OTHER",
-            "To supplier": "",
-            "Quality": ""
-        }
+    
