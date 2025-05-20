8cab98f27331e7400885225833e66147f344a17f
Carlitos1982 <dzecchinel@gmail.com>
2025-05-20 17:51:00 +0200

    Update Oracle_app.py

--- Oracle_app.py
+++ Oracle_app.py
@@ -580,8 +580,28 @@ elif selected_part == "Bolt, Eye":
 elif selected_part == "Bolt, Hexagonal":
     st.subheader("Configurazione - Bolt, Hexagonal")
 
-    bolt_sizes = [ ... ]  # stesso elenco di Bolt, Eye
-    bolt_lengths = [ ... ]
+    bolt_sizes = [
+        "#10-24UNC", "5/16\"-18UNC", "3/8\"-16UNC", "1/2\"-13UNC",
+        "3/4\"-16UNF", "7/8\"-9UNC", "7/8\"-14UNF", "1\"-12UNF",
+        "1-1/8\"-12UNF", "1-1/2\"-12UNC", "2\"-4.5UNC", "2-1/2\"-4UNC",
+        "3\"-6UNC", "4\"-8UNC", "M6x1", "M8x1.25", "M10x1.5", "M12x1.75",
+        "M16x2", "M20x2.5", "M24x3", "M30x3.5", "M36x4", "M42x4.5",
+        "M48x5", "M56x5.5", "M64x6", "M72x6", "M80x6", "M90x6", "M100x6"
+    ]
+
+    bolt_lengths = [
+        "1/8\"in", "1/4\"in", "3/8\"in", "5/16\"in", "1/2\"in",
+        "3/4\"in", "1\"in", "1-1/8\"in", "1-1/4\"in", "1-3/8\"in",
+        "1-1/2\"in", "2\"in", "2-1/8\"in", "2-1/4\"in", "2-3/8\"in",
+        "2-1/2\"in", "2-3/4\"in", "3\"in", "3-1/8\"in", "3-1/4\"in",
+        "3-3/8\"in", "3-1/2\"in", "4\"in", "4-1/8\"in", "4-1/4\"in",
+        "4-3/8\"in", "4-1/2\"in", "50mm", "55mm", "60mm", "65mm",
+        "70mm", "75mm", "80mm", "85mm", "90mm", "95mm", "100mm",
+        "105mm", "110mm", "115mm", "120mm", "125mm", "130mm",
+        "135mm", "140mm", "145mm", "150mm", "155mm", "160mm",
+        "165mm", "170mm", "175mm", "180mm", "185mm", "190mm",
+        "195mm"
+    ]
 
     size_hex   = st.selectbox("Size", bolt_sizes, key="hex_size")
     length_hex = st.selectbox("Length", bolt_lengths, key="hex_length")
@@ -630,7 +650,7 @@ elif selected_part == "Bolt, Hexagonal":
         if note1_hex:
             descr_hex += f", NOTE: {note1_hex}"
         if note2_hex:
-            descr_hex += f", NOTE: {note2_hex}"
+            descr_hex += f", {note2_hex}"
 
         st.session_state["output_data"] = {
             "Item": "56230…",
