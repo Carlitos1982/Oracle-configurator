2fd009c006dc659dfbcf0ab36cc7af278239b56e
Carlitos1982 <dzecchinel@gmail.com>
2025-05-21 12:53:32 +0200

    Update Oracle_app.py

--- Oracle_app.py
+++ Oracle_app.py
@@ -871,6 +871,95 @@ elif selected_part == "Nut, Hex":
             "To supplier": "",
             "Quality": ""
         }
+        
+elif selected_part == "Stud, Threaded":
+    st.subheader("Configurazione - Stud, Threaded")
+
+    threaded_type = st.selectbox("Threaded", ["Partial", "Full"], key="stud_threaded")
+
+    stud_sizes = [
+        "#10-24UNC", "5/16\"-18UNC", "3/8\"-16UNC", "1/2\"-13UNC",
+        "3/4\"-16UNF", "7/8\"-9UNC", "7/8\"-14UNF", "1\"-12UNF",
+        "1-1/8\"-12UNF", "1-1/2\"-12UNC", "2\"-4.5UNC", "2-1/2\"-4UNC",
+        "3\"-6UNC", "4\"-8UNC", "M6x1", "M8x1.25", "M10x1.5", "M12x1.75",
+        "M16x2", "M20x2.5", "M24x3", "M30x3.5", "M36x4", "M42x4.5",
+        "M48x5", "M56x5.5", "M64x6", "M72x6", "M80x6", "M90x6", "M100x6"
+    ]
+
+    stud_lengths = [
+        "1/8\"in", "1/4\"in", "3/8\"in", "5/16\"in", "1/2\"in",
+        "3/4\"in", "1\"in", "1-1/8\"in", "1-1/4\"in", "1-3/8\"in",
+        "1-1/2\"in", "2\"in", "2-1/8\"in", "2-1/4\"in", "2-3/8\"in",
+        "2-1/2\"in", "2-3/4\"in", "3\"in", "3-1/8\"in", "3-1/4\"in",
+        "3-3/8\"in", "3-1/2\"in", "4\"in", "4-1/8\"in", "4-1/4\"in",
+        "4-3/8\"in", "4-1/2\"in", "50mm", "55mm", "60mm", "65mm",
+        "70mm", "75mm", "80mm", "85mm", "90mm", "95mm", "100mm",
+        "105mm", "110mm", "115mm", "120mm", "125mm", "130mm",
+        "135mm", "140mm", "145mm", "150mm", "155mm", "160mm",
+        "165mm", "170mm", "175mm", "180mm", "185mm", "190mm",
+        "195mm"
+    ]
+
+    size_stud = st.selectbox("Size", stud_sizes, key="stud_size")
+    length_stud = st.selectbox("Length", stud_lengths, key="stud_length")
+    note1_stud = st.text_area("Note (opzionale)", height=80, key="stud_note1")
+    dwg_stud = st.text_input("Dwg/doc number", key="stud_dwg")
+
+    mtype_stud = st.selectbox("Material Type", [""] + material_types, key="mtype_stud")
+    pref_df_stud = materials_df[(materials_df["Material Type"] == mtype_stud) & (materials_df["Prefix"].notna())]
+    prefixes_stud = sorted(pref_df_stud["Prefix"].unique()) if mtype_stud != "MISCELLANEOUS" else []
+    mprefix_stud = st.selectbox("Material Prefix", [""] + prefixes_stud, key="mprefix_stud")
+
+    if mtype_stud == "MISCELLANEOUS":
+        names_stud = materials_df[materials_df["Material Type"] == mtype_stud]["Name"].dropna().tolist()
+    else:
+        names_stud = materials_df[
+            (materials_df["Material Type"] == mtype_stud) &
+            (materials_df["Prefix"] == mprefix_stud)
+        ]["Name"].dropna().tolist()
+    mname_stud = st.selectbox("Material Name", [""] + names_stud, key="mname_stud")
+
+    note2_stud = st.text_area("Material Note (opzionale)", height=80, key="stud_note2")
+
+    if st.button("Genera Output", key="gen_stud"):
+        if mtype_stud != "MISCELLANEOUS":
+            materiale_stud = f"{mtype_stud} {mprefix_stud} {mname_stud}".strip()
+            match_stud = materials_df[
+                (materials_df["Material Type"] == mtype_stud) &
+                (materials_df["Prefix"] == mprefix_stud) &
+                (materials_df["Name"] == mname_stud)
+            ]
+        else:
+            materiale_stud = mname_stud
+            match_stud = materials_df[
+                (materials_df["Material Type"] == mtype_stud) &
+                (materials_df["Name"] == mname_stud)
+            ]
+        codice_fpd_stud = match_stud["FPD Code"].values[0] if not match_stud.empty else ""
+
+        descr_stud = f"STUD, THREADED - THREAD: {threaded_type}, SIZE: {size_stud}, LENGTH: {length_stud}"
+        if note1_stud:
+            descr_stud += f", {note1_stud}"
+        descr_stud += f", {materiale_stud}"
+        if note2_stud:
+            descr_stud += f", {note2_stud}"
+
+        st.session_state["output_data"] = {
+            "Item": "56146…",
+            "Description": descr_stud,
+            "Identificativo": "6572-STUD",
+            "Classe ricambi": "",
+            "Categories": "FASCIA ITE 5",
+            "Catalog": "ARTVARI",
+            "Disegno": dwg_stud,
+            "Material": materiale_stud,
+            "FPD material code": codice_fpd_stud,
+            "Template": "FPD_BUY_2",
+            "ERP_L1": "60_FASTENER",
+            "ERP_L2": "12_STANDARD_BOLT_NUT_STUD_SCREW_WASHER",
+            "To supplier": "",
+            "Quality": ""
+        }
 # Output finale
 if "output_data" in st.session_state:
     st.subheader("Risultato finale")
