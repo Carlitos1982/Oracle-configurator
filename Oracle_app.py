004538b7ec1913cd91d9a70a507607c4ea7fc27e
Carlitos1982 <dzecchinel@gmail.com>
2025-08-01 15:05:23 +0200

    Update Oracle_app.py

--- Oracle_app.py
+++ Oracle_app.py
@@ -18,15 +18,7 @@ def render_dataload_panel(item_code_key: str,
     item_code = st.text_input("Item Code", key=item_code_key)
     data = st.session_state.get(state_key, {})
 
-    raw_q = data.get("Quality", "")
-    # Gestione robusta del campo Quality
-    if isinstance(raw_q, list):
-        raw_q = "\n".join(raw_q)
-    elif not isinstance(raw_q, str):
-        raw_q = ""
-
-    raw_q = raw_q.strip()
-
+    raw_q = data.get("Quality", "").strip()
     if not raw_q:
         quality_tokens = ["NA"]
     else:
@@ -124,6 +116,7 @@ def render_dataload_panel(item_code_key: str,
                     file_name=f"update_{item_code}.csv",
                     mime="text/csv"
                 )
+
 
 
 # Caricamento dati materiali da file Excel
