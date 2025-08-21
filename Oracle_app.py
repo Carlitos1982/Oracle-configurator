ee78ccd1facd979364706c81198f79d91d1eace0
Carlitos1982 <dzecchinel@gmail.com>
2025-08-21 17:01:40 +0200

    Add throat bushing pump part

--- Oracle_app.py
+++ Oracle_app.py
@@ -118,6 +118,7 @@ categories = {
         "Diffuser, Pump",
         "Impeller, Pump",
         "Balance Bushing, Pump",
+        "Throat Bushing, Pump",
         "Balance Drum, Pump",
         "Balance Disc, Pump",
         "Shaft, Pump",
@@ -566,6 +567,87 @@ if selected_part == "Balance Drum, Pump":
             update_btn_key="gen_upd_bdrum"
         )
 
+
+# --- THROAT BUSHING, PUMP
+if selected_part == "Throat Bushing, Pump":
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.subheader("✏️ Input")
+        model = st.selectbox("Product Type", [""] + sorted(size_df["Pump Model"].dropna().unique()), key="tbush_model")
+        size_list = size_df[size_df["Pump Model"] == model]["Size"].dropna().tolist()
+        size = st.selectbox("Pump Size", [""] + size_list, key="tbush_size")
+
+        f1_list = features_df[
+            (features_df["Pump Model"] == model) &
+            (features_df["Feature Type"] == "features1")
+        ]["Feature"].dropna().tolist()
+        feature_1 = st.selectbox("Additional Feature 1", [""] + f1_list, key="tbush_feat1") if f1_list else ""
+
+        note = st.text_area("Note", height=80, key="tbush_note")
+        dwg = st.text_input("Dwg/doc number", key="tbush_dwg")
+
+        materiale, codice_fpd, material_note, mtype, mprefix, mname = select_material(
+            materials_df, "tbush"
+        )
+
+        hf_service = st.checkbox("Is it an hydrofluoric acid alkylation service (lethal)?", key="tbush_hf")
+        tmt_service = st.checkbox("TMT/HVOF protection requirements?", key="tbush_tmt")
+        overlay = st.checkbox("DLD, PTAW, Laser Hardening, METCO, Ceramic Chrome?", key="tbush_overlay")
+        hvof = st.checkbox("HVOF coating?", key="tbush_hvof")
+        water = st.checkbox("Water service?", key="tbush_water")
+        stamicarbon = st.checkbox("Stamicarbon?", key="tbush_stamicarbon")
+
+        if st.button("Generate Output", key="tbush_gen"):
+            tag_string, quality = build_quality_tags(
+                {
+                    "hf_service": hf_service,
+                    "tmt_service": tmt_service,
+                    "overlay": overlay,
+                    "hvof": hvof,
+                    "water": water,
+                    "stamicarbon": stamicarbon,
+                }
+            )
+
+            descr_parts = ["THROAT BUSHING, PUMP"]
+            for val in [model, size, feature_1, note, materiale, material_note]:
+                if val:
+                    descr_parts.append(val)
+            descr = "*" + " - ".join(descr_parts) + " " + tag_string
+
+            st.session_state["output_data"] = {
+                "Item": "40223…",
+                "Description": descr,
+                "Identificativo": "4132-NECK BUSH",
+                "Classe ricambi": "1-2-3",
+                "Categories": "FASCIA ITE 4",
+                "Catalog": "ARTVARI",
+                "Disegno": dwg,
+                "Material": materiale,
+                "FPD material code": codice_fpd,
+                "Template": "FPD_BUY_1",
+                "ERP_L1": "20_TURNKEY_MACHINING",
+                "ERP_L2": "16_BUSHING",
+                "To supplier": "",
+                "Quality": quality
+            }
+
+    with col2:
+        st.subheader("📤 Output")
+        if "output_data" in st.session_state:
+            for k, v in st.session_state["output_data"].items():
+                if k in ["Quality", "To supplier", "Description"]:
+                    st.text_area(k, value=v, height=200)
+                else:
+                    st.text_input(k, value=v)
+
+    with col3:
+        render_dataload_panel(
+            item_code_key="tbush_item_code",
+            create_btn_key="gen_dl_tbush",
+            update_btn_key="gen_upd_tbush"
+        )
 
 
 # --- NUT, IMPELLER
